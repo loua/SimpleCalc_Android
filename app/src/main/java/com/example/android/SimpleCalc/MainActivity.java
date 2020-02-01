70ce088eb62213ff83d1a3802de9de7d874bee6f
Lou Aloia <lou.aloia@gmail.com>
2020-02-01 13:52:07 -0600

    Lowercase package name and add dependencies for ViewModel.

--- app/src/main/java/com/example/android/SimpleCalc/MainActivity.java
+++ app/src/main/java/com/example/android/SimpleCalc/MainActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.SimpleCalc;
+package com.example.android.simplecalc;
 
 import android.os.Bundle;
 import android.util.Log;
--- app/src/test/java/com/example/android/simplecalc/CalculatorTest.java
+++ app/src/test/java/com/example/android/simplecalc/CalculatorTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.SimpleCalc;
+package com.example.android.simplecalc;
 
 
 import org.junit.Before;
